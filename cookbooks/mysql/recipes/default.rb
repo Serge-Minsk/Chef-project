b63e6634df5d6083e3d4289a5565d2d972167efe
Serge-Minsk <s.v.dirman@gmail.com>
2016-01-21 00:39:28 +0300

    mysql cookbook modifed

--- cookbooks/mysql/recipes/default.rb
+++ cookbooks/mysql/recipes/default.rb
@@ -6,3 +6,9 @@
 #
 # All rights reserved - Do Not Redistribute
 #
+mysql_service node['mysql_hostname'] do
+  port node['port']
+  version '5.5'
+  initial_root_password 'change me'
+  action [:create, :start]
+end
